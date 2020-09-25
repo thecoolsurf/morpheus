db31978dd72142fb30e2557c8cf214915d9a3e6c
hubert <hubert@hubert-ubuntu>
2020-09-25 09:16:27 +0200

    update test #3

--- morpheus/src/Converter/XMLConverter.php
+++ morpheus/src/Converter/XMLConverter.php
@@ -6,32 +6,31 @@ use DOMDocument;
 
 class XMLConverter
 {
-    
+
     public static function xmlToArray(string $filepath): array
     {
         $result = [];
         if (file_exists($filepath)) {
             $DOM = new DOMDocument();
             $document = $DOM::load($filepath, 0);
-            var_dump($document);
-            $datas = $document->getElementsByTagName('job');
+            $datas = $document->getElementsByTagName('jobs');
             foreach ($datas as $item) {
                 foreach ($item->childNodes as $nodes) {
                     $name = $nodes->nodeName;
                     $value = $nodes->nodeValue;
+                    var_dump($name);
+                    var_dump($value);
+                    array_push($result, [
+                        'name' => $name,
+                        'value' => $value,
+                    ]);
                 }
-                var_dump($name);
-                var_dump($value);
-                array_push($result, [
-                    'name' => $name,
-                    'value' => $value,
-                ]);
             }
         } else {
             die('XML file not found');
         }
+        var_dump($result);
         return $result;
     }
-    
-}
 
+}
